24fad7061fe868943e94492877f66594a0d84f96
Nikita <qbeboom@.mail.ru>
2020-02-20 00:01:37 +0800

    Добавили в структуру HTML модальное окно, подключили подальное окно через файл js, и повешали оброботчики событий

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+/* Создаем переменную с дом элементом, 
+можно обратится к тегам, классам, идентификаторам */
+var button = document.querySelector('#button');
+var modal = document.querySelector('#modal');
+var close = document.querySelector('#close');
+
+/* Добавляем отслеживатель событий на кнопку button, 
+отловили событие клика на кнопку и повешали модальное окно*/
+button.addEventListener('click', function() {
+    modal.classList.add('modal_active');
+});
+
+/* Теперь необходимо снова отловить клик на 
+крестик и повешать на него закрытие модального окна */
+
+close.addEventListener('click', function(){
+    modal.classList.remove('modal_active');
+});
